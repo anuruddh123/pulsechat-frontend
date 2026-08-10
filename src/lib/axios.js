16e15bdac59a199ad6834a01d19de81bebebabb2
anuruddh123 <anuruddhtiwari2022@gmail.com>
2026-08-10 21:43:24 +0530

    fix axios

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
+const baseURL =
+  import.meta.env.MODE === "development"
+    ? "https://pulsechat-backend-h4qt.onrender.com/api"
+    : import.meta.env.VITE_API_URL || "https://pulsechat-backend-h4qt.onrender.com/api";
+
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://pulsechat-backend-h4qt.onrender.com/api" : "/api",
+  baseURL,
   withCredentials: true,
 });
